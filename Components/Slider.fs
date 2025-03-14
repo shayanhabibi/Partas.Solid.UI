f7d1cd24339a5bd6cff8e8eb2ae53f0733cd66b7
cabboose <shayanftw@gmail.com>
2025-03-14 18:15:32 +0800

    Slider: Complete implementation

--- Components/Slider.fs
+++ Components/Slider.fs
@@ -2,6 +2,7 @@
 
 open Partas.Solid
 open Partas.Solid.Kobalte
+open Partas.Solid.Polymorphism
 open Fable.Core
 
 [<Erase>]
@@ -26,24 +27,35 @@ type SliderTrack() =
         )   .spread props
 
 [<Erase>]
+type SliderThumb() =
+    inherit Slider.Thumb()
+    [<SolidTypeComponentAttribute>]
+    member props.constructor =
+        Slider.Thumb(
+            class' = Lib.cn [|
+                "top-[-6px] block size-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+                props.class'
+            |]
+        ).spread props { Slider.Input() }
+
+[<Erase>]
 type SliderFill() =
     inherit Slider.Fill()
     [<SolidTypeComponentAttribute>]
     member props.constructor =
-        Slider.Thumb(class' = Lib.cn [|
-            "top-[-6px] block size-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+        Slider.Fill(class' = Lib.cn [|
+            "absolute h-full rounded-full bg-primary"
             props.class'
         |]).spread(props)
-            { Slider.Input() }
 
 [<Erase>]
 type SliderLabel() =
-    inherit Label()
+    inherit Slider.Label()
     [<SolidTypeComponentAttribute>]
-    member props.constructor = Label().spread props
+    member props.constructor = Slider.Label().as'(!@Label).spread props
 
 [<Erase>]
 type SliderValueLabel() =
     inherit Slider.ValueLabel()
     [<SolidTypeComponentAttribute>]
-    member props.constructor = Slider.ValueLabel().spread props
+    member props.constructor = Slider.ValueLabel().as'(!@Label).spread props
