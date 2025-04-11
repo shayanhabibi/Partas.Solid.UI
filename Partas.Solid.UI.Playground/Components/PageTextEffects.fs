fe05bc10ce9c72a7850581f49fc64e9c4f995942
cabboose <shayanftw@gmail.com>
2025-04-11 16:52:31 +0800

    enable border-beam in test

--- Partas.Solid.UI.Playground/Components/PageTextEffects.fs
+++ Partas.Solid.UI.Playground/Components/PageTextEffects.fs
@@ -34,8 +34,8 @@ type PageTextEffects() =
             
             WordFadeIn(text = "Word Fade In", class' = textClass)
             
-            // ShineBorder(
-            //     class' = "relative flex h-(500px) w-full items-center justify-center overflow-hidden rounded-lg shadow-lg",
-            //     color = !![| "#A07CFE"; "#FE8FB5"; "#FFBE7B" |]
-            // ) {span(class' = textClass) { "Shine Border" }}
+            ShineBorder(
+                class' = "relative flex h-[500px] w-full items-center justify-center overflow-hidden rounded-lg shadow-lg",
+                color = !![| "#A07CFE"; "#FE8FB5"; "#FFBE7B" |]
+            ) {span(class' = textClass) { "Shine Border" }}
         }
